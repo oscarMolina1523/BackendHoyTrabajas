3b5fd4e6a9e3eea94c42b8e264c79773fc000c10
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-06 19:57:11 -0600

    refactor: update app file

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cartRoutes from './routes/cartRoutes';
 
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './docs/swagger';
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -12,5 +13,11 @@ app.use(express.json());
 app.use('/api', productRoutes);
 app.use('/api', cartRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  console.log(`Swagger en http://localhost:${PORT}/api-docs`);
+});
 
 export default app;
--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app';
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-  console.log(`Swagger en http://localhost:${PORT}/api-docs`);
-});
