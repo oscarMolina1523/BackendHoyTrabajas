bb57455b5c1c743b036909dd1895354b21274e55
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-06 19:06:50 -0600

    feat: create a route for cart

---
+++ src/routes/cartRoutes.ts
@@ -0,0 +1,39 @@
+import { Router } from 'express';
+import { addToCart, getCart } from '../controllers/cartController';
+
+const router = Router();
+
+/**
+ * @swagger
+ * /api/cart:
+ *   post:
+ *     summary: Agrega un producto al carrito
+ *     tags: [Carrito]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               productId:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Producto agregado
+ */
+router.post('/cart', addToCart);
+
+/**
+ * @swagger
+ * /api/cart:
+ *   get:
+ *     summary: Obtiene productos en el carrito
+ *     tags: [Carrito]
+ *     responses:
+ *       200:
+ *         description: Lista del carrito
+ */
+router.get('/cart', getCart);
+
+export default router;
