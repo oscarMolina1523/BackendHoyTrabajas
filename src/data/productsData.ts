3073ed0b3aa4d0fd46cdafb9ea2dbfe856a14125
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-07 07:54:21 -0600

    fix: solve data problem

--- src/data/productsData.ts
+++ src/data/productsData.ts
@@ -1,7 +1,74 @@
 import Product from "../models/Product";
 
 export const productsData: Product[] = [
-  { id: "1", name: 'Producto 1', price: 100 },
-  { id: "2", name: 'Producto 2', price: 200 }
+  {
+    id: "1",
+    name: "Laptop HP 15",
+    description: "Laptop con procesador AMD Ryzen 5, 8GB RAM y SSD de 256GB.",
+    price: 549.99,
+    image: "https://cdn.cs.1worldsync.com/syndication/feeds/hp/inline-content/KV/9/2/E/C/2/92EC265F083B559D9B274A17304FF34FE4EC790C_w_400_hero.jpg"
+  },
+  {
+    id: "2",
+    name: "Auriculares Bluetooth",
+    description: "Auriculares inalámbricos con cancelación de ruido y micrófono.",
+    price: 79.99,
+    image: "https://simpletechnic.com/cdn/shop/files/AuricularesPro06_2.jpg?v=1689910988&width=1445"
+  },
+  {
+    id: "3",
+    name: "Monitor Samsung 24",
+    description: "Monitor LED Full HD de 24 pulgadas con HDMI y VGA.",
+    price: 129.00,
+    image: "https://res.cloudinary.com/cdnco/image/upload/b_white,c_pad,g_center,h_800,q_90/v1/omarberrio/imgs/sQXn6dCNSqxQCr6EzruXt8Rq.webp"
+  },
+  {
+    id: "4",
+    name: "Mouse Gamer Logitech",
+    description: "Mouse óptico con DPI ajustable y luces RGB.",
+    price: 39.50,
+    image: "https://m.media-amazon.com/images/I/61mpMH5TzkL._AC_SL1500_.jpg"
+  },
+  {
+    id: "5",
+    name: "Teclado Mecánico Redragon",
+    description: "Teclado mecánico retroiluminado con switches azules.",
+    price: 59.99,
+    image: "https://images-cdn.ubuy.qa/67081c19844f384d26521c26-redragon-k552-mechanical-gaming-keyboard.jpg"
+  },
+  {
+    id: "6",
+    name: "Tablet Lenovo M10",
+    description: "Tablet 10.1'' Android con 64GB de almacenamiento.",
+    price: 199.00,
+    image: "https://http2.mlstatic.com/D_NQ_NP_629682-MLU78712364963_082024-O.webp"
+  },
+  {
+    id: "7",
+    name: "Impresora HP DeskJet",
+    description: "Impresora multifunción color con WiFi.",
+    price: 89.99,
+    image: "https://www.radioshackla.com/media/catalog/product/h/p/hp_deskjet_ink_advantage_2375_6_v7ketyots0ompgjj.png?optimize=medium&bg-color=255,255,255&fit=bounds&height=&width=&canvas=:&format=jpeg"
+  },
+  {
+    id: "8",
+    name: "Smartwatch Xiaomi Mi Band 8",
+    description: "Pulsera inteligente con monitoreo de salud y pantalla AMOLED.",
+    price: 49.99,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq6Up5G1XZ-fPEtNCLt812aKh077b9XEt0aw&s"
+  },
+  {
+    id: "9",
+    name: "Disco Duro Externo 1TB",
+    description: "Disco duro portátil USB 3.0 para copias de seguridad.",
+    price: 69.90,
+    image: "https://s3.amazonaws.com/online.storage/SEVASA/Products/e8f658a8-10ff-41c5-9f8d-dfb893ceb549.jpg"
+  },
+  {
+    id: "10",
+    name: "Silla Ergonomica de Oficina",
+    description: "Silla ajustable con soporte lumbar y reposabrazos.",
+    price: 149.99,
+    image: "https://michelpierson.com/oficina/wp-content/uploads/2017/09/Silla-semi-ejecutiva-Modelo-A7M-NPC1-min.jpg"
+  }
 ];
-
